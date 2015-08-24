1125047f1b9a8a2224bbc250ecf6cd2efd4b428f
mengchun.bian <58070368@qq.com>
2015-08-24 15:32:19 +0800

    增加userinfo用户不存在判断

--- application/controllers/api/userinfo.php
+++ application/controllers/api/userinfo.php
@@ -27,6 +27,10 @@ if(isset($_GET['uid'])||$_GET['tel'])
 		exit;
 	} 
 	$userinfo = $re->userinfo($uid);//个人信息
+	if(!$userinfo)
+	{
+		echo $msg->show(405);
+	}
 	$parentpercent = $re->parentpercent($uid);//父母信息完成比例
 	$likeNum = $re->likeNum($uid);//收藏个数
 	$data = array('userinfo'=>$userinfo,
