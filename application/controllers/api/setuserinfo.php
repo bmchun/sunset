f1c807e2abad950aa559f02b78f4d28d518f71e1
mengchun.bian <58070368@qq.com>
2015-08-24 23:09:38 +0800

    修复第三方注册问题

--- application/controllers/api/setuserinfo.php
+++ application/controllers/api/setuserinfo.php
@@ -85,9 +85,8 @@ if(isset($_POST['data']))
 		}
 		else //如果用户不存在，创建新用户
 		{
-			//$setUser->userinfo_select($arr_select,1);
-			echo 123;
-			if($re ==1)
+			$re = $setUser->userinfo_insert($arr);
+			if(mysql_fetch_array($re))
 				echo $res->show(200,mysql_fetch_assoc($setUser->userinfo_select($arr_select,1)));
 			else
 				echo $res->show(401);
