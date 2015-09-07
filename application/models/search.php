b6e8b6f59f10a4935b8961c8049c831228955ca5
mengchun.bian <58070368@qq.com>
2015-09-07 14:16:08 +0800

    搜索翻页

--- application/models/search.php
+++ application/models/search.php
@@ -11,7 +11,7 @@ class Search {
 	}
 	function search($key,$uid)
 	{
-		$string = "`describe` LIKE '%$key%'";
+		$string = "`describe` LIKE '%$key%' ORDER BY `id`";
 		$re = new ItemInfo();
 		$data = $re->iteminfo_select_like($string, 4);
 		return $data;
